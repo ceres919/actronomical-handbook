3bca6ed3898cc9b61cdade5aeb5733d786411956
Yana A. Kosteva <kosteva@protei-lab.ru>
2024-09-19 04:20:53 +0700

    feat: Add news banner view

--- app/src/main/java/com/example/actronomicalhandbook/MainActivity.kt
+++ app/src/main/java/com/example/actronomicalhandbook/MainActivity.kt
@@ -4,24 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.AbsoluteCutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.delay
 import kotlin.random.Random
 
 data class NewsItem(
@@ -70,6 +77,27 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview
+@Composable
+private fun NewsBanner(viewModel: NewsViewModel = viewModel()) {
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(5000)
+            viewModel.reorderNews()
+        }
+    }
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(Modifier.weight(1f)) {
+            NewsCard(modifier = Modifier.weight(1f), newsItem = viewModel.displayedNews[0], onLike = { viewModel.likeNewsCb(it) })
+            NewsCard(modifier = Modifier.weight(1f), newsItem = viewModel.displayedNews[1], onLike = { viewModel.likeNewsCb(it) })
+        }
+        Row(Modifier.weight(1f)) {
+            NewsCard(modifier = Modifier.weight(1f), newsItem = viewModel.displayedNews[2], onLike = { viewModel.likeNewsCb(it) })
+            NewsCard(modifier = Modifier.weight(1f), newsItem = viewModel.displayedNews[3], onLike = { viewModel.likeNewsCb(it) })
+        }
+    }
+}
+
 @Composable
 private fun NewsCard(
     modifier: Modifier = Modifier,
@@ -92,18 +120,24 @@ private fun NewsCard(
                     .fillMaxSize()
                     .padding(16.dp)
             ) {
-                Text(
-                    text = "${newsItem.id}. ${newsItem.summary}",
-                    color = Color.White,
-                    style = MaterialTheme.typography.bodyLarge
-                )
-                Spacer(modifier = Modifier.weight(1f))
+                Box(
+                    modifier = Modifier.weight(9f),
+                ) {
+                    Text(
+                        text = "${newsItem.id}. ${newsItem.summary}",
+                        color = Color.White,
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
                 Box(
                     modifier = Modifier
-                        .background(Color.Cyan)
+                        .background(Color.Transparent, RoundedCornerShape(13.dp))
+                        .border(5.dp, Color.LightGray, RoundedCornerShape(15.dp))
+                        .width(80.dp)
+                        .weight(1f)
                         .clickable { onLike(newsItem) }
-                        .padding(8.dp),
-                    contentAlignment = Alignment.Center
+                        .padding(10.dp),
+                    contentAlignment = Alignment.CenterEnd
                 ) {
                     Text(
                         text = "${newsItem.likeCount} \uD83D\uDC7D",
