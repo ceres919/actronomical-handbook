1f8fcde0670618d1537ce648230562cecd54d8d1
Yana A. Kosteva <kosteva@protei-lab.ru>
2024-09-19 03:30:05 +0700

    feat: Add news card view

--- app/src/main/java/com/example/actronomicalhandbook/MainActivity.kt
+++ app/src/main/java/com/example/actronomicalhandbook/MainActivity.kt
@@ -3,11 +3,69 @@ package com.example.actronomicalhandbook
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.AbsoluteCutCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+data class NewsItem(
+    val id: Int,
+    val summary: String,
+    var likeCount: Int = 0,
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+        }
+    }
+}
+
+@Composable
+private fun NewsCard(modifier: Modifier = Modifier, newsItem: NewsItem, onLike: (NewsItem) -> Unit) {
+    Card(
+        shape = AbsoluteCutCornerShape(10.dp),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(2.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.DarkGray)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "${newsItem.id}. ${newsItem.summary}",
+                    color = Color.White,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Box(
+                    modifier = Modifier
+                        .background(Color.Cyan)
+                        .clickable { onLike(newsItem) }
+                        .padding(8.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "${newsItem.likeCount} \uD83D\uDC7D",
+                        color = Color.Gray,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
         }
     }
 }
